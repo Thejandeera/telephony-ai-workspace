02f19ff1191fe22e9dbd1a9cc6b51d0b1f3cedb9
Sandeepana K M T <137618345+Thejandeera@users.noreply.github.com>
2026-08-03 15:07:14 +0530

    feat: added call-event microservice

---
+++ call-event/src/main/java/com/example/call_event/CallEventApplication.java
@@ -0,0 +1,13 @@
+package com.example.call_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallEventApplication.class, args);
+	}
+
+}
